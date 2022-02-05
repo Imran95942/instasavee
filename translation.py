925ca49f168252f62e91c7afacae844d3ac4cc23
Imran95942 <89960334+Imran95942@users.noreply.github.com>
2022-02-05 17:08:58 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,9 +1,6 @@
 class Translation(object):
     START_TEXT = """Здравствуйте {},
-I'm All URL Uploader!
-Привет, при помощи этого бота вы сможете скачать с Instagram, YouTube, TikTok и т.д.
-Support Sites <a href="https://ytdl-org.github.io/youtube-dl/supportedsites.html">HERE</a>
-/help for more details!"""
+Привет, при помощи этого бота вы сможете скачать с Instagram, YouTube, TikTok и т.д."""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
